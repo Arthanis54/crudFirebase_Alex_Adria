aff556920a8dc2141900970fcd5d0a3e41cf0301
Adria <a2208022@institutmontilivi.cat>
2024-03-04 18:51:38 +0100

    Agregar archivos de proyecto.

---
+++ crudFirebase_Alex_Adria/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace crudFirebase_Alex_Adria
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
